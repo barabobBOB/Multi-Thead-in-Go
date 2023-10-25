db915f644dcd803652acd078279307dd514b4ae2
Seyeon_Choi <choi20014830@gmail.com>
2023-10-26 05:05:12 +0900

    feat: 커맨드 입력

---
+++ input.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+func InputCommend() (string, string) {
+	scanText := bufio.NewScanner(os.Stdin)
+	scanText.Scan()
+	commend := scanText.Text()
+	commandList := strings.Split(commend, " ")
+
+	if commandList[0] != "dgrep" {
+		panic("명령어가 올바르지 않습니다.")
+	}
+
+	return commandList[1], commandList[2]
+}
